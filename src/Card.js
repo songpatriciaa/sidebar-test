ca08f619179b356a83ab827f900ce2596440cfa9
Patricia <patriciasong2020@u.northwestern.edu>
2018-11-07 19:33:37 -0600

    css for app & card
    - adjusted margins, added flex
    - button changed to look like a highlighted word
    - changed modal so it doesn't close when you click outside the card

--- src/Card.js
+++ src/Card.js
@@ -8,7 +8,9 @@ class Card extends Component {
 
     // this.state.text = this.props.text;
     this.state = {
-      open: false
+      open: false,
+      buttonX: null,
+      buttonY: null
     };
   }
 
@@ -16,15 +18,39 @@ class Card extends Component {
     this.setState({ open: true });
   }
 
-  onCloseModal = () => {
-    this.setState({ open: false });
+  onCloseModal = (event) => {
+    if (event.className.baseVal) {
+      this.setState({ open: false });
+    }
+
   };
+
+  changeColor = (button) => {
+    button.style.background = "#FFDAB9";
+    button.style.borderColor = "#FFDAB9";
+  }
+
+  getButtonPos = (button) => {
+    this.setState({
+      buttonX: button.left,
+      buttonY: button.top
+    });
+  }
+
+  changeModalPos = (refName,event) => {
+    console.log("hi");
+    if (this.state.buttonY) {
+      this.refs[refName].style.color = "blue";
+    }
+  }
+
+  // () => {this.onOpenModal(); this.changeModalPos.bind(this,"modal1")}
 
   render() {
     return (
       <div className="Context">
-        <button onClick={this.onOpenModal}>{this.props.children}</button>
-        <Modal open={this.state.open} onClose={this.onCloseModal} center>
+        <button onClick={(event) => {this.onOpenModal(); this.changeColor(event.target); this.getButtonPos(event.target);}}>{this.props.children}</button>
+        <Modal ref="modal1" open={this.state.open} onOpen={() => {this.changeModalPos.bind(this,"modal1")}} onClose={(event) => {this.onCloseModal(event.target)}} center>
           <h2>Simple centered modal</h2>
         </Modal>
       </div>
