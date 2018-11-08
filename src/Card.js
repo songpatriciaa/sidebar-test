aeefc91422c1fa1b4dc960482f86202d15b3869d
Patricia <patriciasong2020@u.northwestern.edu>
2018-11-07 21:17:40 -0600

    functionality implemented
    - multiple cards
    - position of cards fixed
    - made our own modal

--- src/Card.js
+++ src/Card.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import Modal from 'react-responsive-modal';
 import './Card.css';
 
 class Card extends Component {
@@ -10,49 +9,71 @@ class Card extends Component {
     this.state = {
       open: false,
       buttonX: null,
-      buttonY: null
+      buttonY: null,
+      styles: {
+        display:"none",
+        background: "red"
+      }
     };
   }
 
-  onOpenModal = () => {
-    this.setState({ open: true });
+  componentDidMount() {
+    this.setState({
+      buttonX: this.refs.button.left,
+      buttonY: this.refs.button.top
+    });
+    console.log(this.state.buttonX);
+    console.log(this.state.buttonY);
   }
 
-  onCloseModal = (event) => {
-    if (event.className.baseVal) {
-      this.setState({ open: false });
-    }
+  onOpenModal = () => {
+    this.setState({
+      open: true,
+      styles: {
+        background: "red",
+        display: "block"
+      }
+    });
+  }
 
-  };
+  onCloseModal = () => {
+    this.setState({
+      open:false,
+      styles: {
+        display: "none"
+      }
+    });
+  }
 
   changeColor = (button) => {
     button.style.background = "#FFDAB9";
     button.style.borderColor = "#FFDAB9";
   }
 
-  getButtonPos = (button) => {
-    this.setState({
-      buttonX: button.left,
-      buttonY: button.top
-    });
-  }
-
-  changeModalPos = (refName,event) => {
-    console.log("hi");
-    if (this.state.buttonY) {
-      this.refs[refName].style.color = "blue";
-    }
-  }
+  // getButtonPos = (button) => {
+  //   this.setState({
+  //     buttonX: button.left,
+  //     buttonY: button.top
+  //   });
+  // }
 
   // () => {this.onOpenModal(); this.changeModalPos.bind(this,"modal1")}
+
+
+  // <Modal style={this.state.styles} ref="modal1" open={this.state.open} onClose={(event) => {this.onCloseModal(event.target)}} center>
+  //   <h2>Simple centered modal</h2>
+  // </Modal>
+
 
   render() {
     return (
       <div className="Context">
-        <button onClick={(event) => {this.onOpenModal(); this.changeColor(event.target); this.getButtonPos(event.target);}}>{this.props.children}</button>
-        <Modal ref="modal1" open={this.state.open} onOpen={() => {this.changeModalPos.bind(this,"modal1")}} onClose={(event) => {this.onCloseModal(event.target)}} center>
-          <h2>Simple centered modal</h2>
-        </Modal>
+        <button ref="button" onClick={(event) => {this.onOpenModal(); this.changeColor(event.target);}}>{this.props.children}</button>
+          <div className="modal" style={this.state.styles}>
+            <button type="button" onClick={this.onCloseModal} className="close">&times;</button>
+            <h2>{this.props.modalContent}</h2>
+          </div>
+
       </div>
     );
   }
