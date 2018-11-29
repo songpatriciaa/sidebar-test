79ceadb6533cad2be7619fd3ba9648014c57796f
Patricia <patriciasong2020@u.northwestern.edu>
2018-11-29 15:23:00 -0600

    mobile modals added
    - changed to underline
    - need to do much more

--- src/Card.js
+++ src/Card.js
@@ -4,7 +4,11 @@ import './Card.css';
 class Card extends Component {
   constructor(props) {
     super(props);
-
+    this.classes = {
+      'mobileModal': true,
+      'overlay': true,
+      'overlayCenter': true
+    }
     // this.state.text = this.props.text;
     this.state = {
       open: false,
@@ -43,9 +47,16 @@ class Card extends Component {
     });
   }
 
-  changeColor = (button) => {
-    button.style.background = "#FFDAB9";
-    button.style.borderColor = "#FFDAB9";
+  // changeColor = (button) => {
+  //   button.style.background = "#FFDAB9";
+  //   button.style.borderColor = "#FFDAB9";
+  // }
+
+  classNames = (classes) => {
+    return Object.entries(classes)
+      .filter(([key,value]) => value)
+      .map(([key, value]) => key)
+      .join(' ');
   }
 
   // getButtonPos = (button) => {
@@ -66,10 +77,10 @@ class Card extends Component {
   render() {
     return (
       <div className="Context">
-        <button ref="button" onClick={(event) => {this.onOpenModal(); this.changeColor(event.target);}}>{this.props.children}</button>
+        <button ref="button" onClick={(event) => this.onOpenModal()}>{this.props.children}</button>
           <div className="modal" style={this.state.styles}>
             <span type="button" onClick={this.onCloseModal} className="close">&times;</span>
-            <div className="modalContent">{this.props.modalContent}</div>
+              <div className="modalContent">{this.props.modalContent}</div>
           </div>
 
       </div>
